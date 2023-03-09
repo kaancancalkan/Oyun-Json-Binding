4f02d6a00cf8164de0fabeaabd79833fbb067be5
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-03-09 18:20:54 +0300

    Added Filter But Not Working

--- mertcalkam/webapp/controller/Main.controller.js
+++ mertcalkam/webapp/controller/Main.controller.js
@@ -12,10 +12,36 @@ sap.ui.define([
             onInit: function () {
                 var oModel = this.getOwnerComponent().getModel("Wolfteam");
                 oModel.loadData(sap.ui.require.toUrl("json/mertcalkam/model/wolfteam.json"));
+
                 
 
 
 
-            }
+            },
+            handleSelectionChange: function(oEvent) {
+                const oTable = this.byId("charactersTable");
+                const oBinding = oTable.getBinding("items");
+                const aFilters = [];
+              
+                // Get the selected side(s) from the MultiComboBox
+                const aSelectedSides = oEvent.getSource().getSelectedKeys();
+              
+                // Create a filter based on the selected side(s)
+                if (aSelectedSides.length > 0) {
+                  const oFilter = new sap.ui.model.Filter("Side", sap.ui.model.FilterOperator.EQ, aSelectedSides);
+                  aFilters.push(oFilter);
+                }
+              
+                // Apply filters to the binding
+                oBinding.filter(aFilters);
+                console.log("Alert")
+              }
+              
+             
+              
+              
+              
+              
+              
         });
     });
